132865c8ec7ad1f6a0a441e0fbc5005b4f4bf270
david hollenbeck <61215923+davidhollenbeckx@users.noreply.github.com>
2024-11-15 06:50:04 -0800

    adding optional toast impl - pass in action data
    useEffect feels so good when theres not a youtuber in your ear saying thats an antipattern

--- AppBridgeV4Modal.tsx
+++ AppBridgeV4Modal.tsx
@@ -52,12 +52,18 @@ export function useAppBridgeV4Modal({
     }, [id]);
 
     useEffect(() => {
-        function handleMessageFromModal(ev: MessageEvent<{ dirty: boolean }>) {
+        function handleMessageFromModal(ev: MessageEvent<{ dirty: boolean, actionSuccess?: boolean, message?: string }>) {
             if (ev.data.dirty) {
                 setDirty(true);
             }
             if (ev.data?.dirty === false) {
                 setDirty(false);
+            }
+            if (ev.data?.actionSuccess === true) {
+                shopify.toast.show(ev.data.message ?? "Success")
+            }
+            if (ev.data?.actionSuccess === false) {
+                shopify.toast.show(ev.data.message ?? "Failed", { isError: true })
             }
         }
 
@@ -94,14 +100,48 @@ export function useInnerAppBridgeV4Modal({
     onSave,
     onReset,
     dirty,
+    actionData,
 }: {
     onSave: () => void;
     onReset: () => void;
     dirty: boolean;
+    actionData?: RouteResponse<{ message: string }>;
 }) {
+    const [allowToastOnIdle, setAllowToastOnIdle] = useState<boolean>(
+        actionData !== undefined,
+    );
+    const navigateState = useNavigateState();
     const toggleIsDirty = useCallback((dirty: boolean) => {
-        window.opener.postMessage({ dirty }, location.origin);
+        window.opener?.postMessage({ dirty }, location.origin);
     }, []);
+    useEffect(() => {
+        if (navigateState !== "idle") {
+            if (!allowToastOnIdle) {
+                setAllowToastOnIdle(true);
+            }
+            return;
+        }
+        if (actionData?.message && typeof actionData?.success === "boolean") {
+            if (actionData?.success) {
+                window.opener?.postMessage(
+                    {
+                        actionSuccess: true,
+                        message: actionData?.message,
+                    },
+                    location.origin,
+                );
+            } else {
+                window.opener?.postMessage(
+                    {
+                        actionSuccess: false,
+                        message: actionData?.message,
+                    },
+                    location.origin,
+                );
+            }
+            setAllowToastOnIdle(false);
+        }
+    }, [actionData?.message, actionData?.success, navigateState, onReset]);
     useEffect(() => {
         if (dirty) {
             toggleIsDirty(true);
@@ -123,5 +163,5 @@ export function useInnerAppBridgeV4Modal({
         return () => {
             window.removeEventListener("message", handleMessageFromMainApp);
         };
-    }, []);
+    }, [onSave, onReset]);
 }
